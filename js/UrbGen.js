a2007e4669b87123378f18d65eeca6d7d013926a
N Robb <shogastudio@gmail.com>
2014-07-10 12:08:01 +0100

    Implement relative/absolute edges and relative/absolute points

--- js/UrbGen.js
+++ js/UrbGen.js
@@ -1,20 +1,59 @@
+// Defines an abolute point, specified by a vec3
+var AbsPoint = function(vec) {
+  this.vec = vec;
+  this.x = vec.x;
+  this.y = vec.y;
+  this.z = vec.z;
+};
+// Defines a relative point, specified by a line and a value 0 - 1
+var RelPoint = function(edge, r) {
+  this.vec = edge.getPoint(r).vec;
+  this.x = vec.x;
+  this.y = vec.y;
+  this.z = vec.z;
+};
 // Defines a Vector
 var Vec3 = function(x, y, z) {
   this.x = x;
   this.y = y;
   this.z = z;
 };
-// Defines a Road Segment
-var RoadSegment = function(start, end, width) {
-  this.start = start;
-  this.end = end;
-  this.width = width;
-  this.length = getLength(this);
+// Defines an absolute edge, specified by a start and end vec3
+var AbsEdge = function(vec3Start, vec3End) {
+  this.start = new AbsPoint(vec3Start);
+  this.end = new AbsPoint(vec3End);
+  this.getPoint = function(r) {
+    return linearInterpolate(this, r);
+  };
 };
-// Defines a Block
-var Block = function(roadSegment1, roadSegment2, roadSegment3, roadSegment4) {
-  this.edges = [roadSegment1, roadSegment2, roadSegment3, roadSegment4];
+// Defines a relative edge, specified by a master edge, and start and end values
+var RelEdge = function(master, value0_1Start, value0_1End) {
+  this.start = master.getPoint(value0_1Start);
+  this.end = master.getPoint(value0_1End);
+  this.master = master;
+  this.getPoint = function(r) {
+    return master.getPoint(this.start + r / 10);
+  };
 };
+// Defines a Quadrilateral, specified by four edges
+var Quad = function(edge1, edge2, edge3, edge4) {
+  this.edges = [edge1, edge2, edge3, edge4];
+};
+// Finds a point on an edge using linear interpolation
+var linearInterpolate = function(edge, r) {
+  var x = (1 - r) * edge.start.x + r * edge.end.x;
+  var y = (1 - r) * edge.start.y + r * edge.end.y;
+  var z = (1 - r) * edge.start.z + r * edge.end.z;
+  return new AbsPoint(new Vec3(x, y, z));
+};
+// Finds the length of an edge
+var getLength = function(edge) {
+  return Math.sqrt(Math.pow(edge.end.x - edge.start.x, 2)
+    + Math.pow(edge.end.y - edge.start.y, 2));
+};
+////////////////////////////////////////////////////////////////////////////////
+/* DEPRECATED
+////////////////////////////////////////////////////////////////////////////////
 // Initializes the vertices array
 var vertices = [];
 // Initializes the roads array
@@ -64,13 +103,16 @@ var getLength = function(roadSegment) {
     - vertices[roadSegment.start].y, 2));
 };
 // Finds a random point on a line
-var getPointOnLine = function(roadSegment) {
+var getPointOnLine = function(roadSegment, r) {
 	var xLength = vertices[roadSegment.end].x - vertices[roadSegment.start].x;
 	var yLength = vertices[roadSegment.end].y - vertices[roadSegment.start].y;
-	var r = (Math.random() * 2 + 4) / 10;
+	if (isNaN(r)) {
+	  r = (Math.random() * 2 + 4) / 10;
+	}
 	var point = new Vec3(Math.floor(vertices[roadSegment.start].x + xLength * r),
 	  Math.floor(vertices[roadSegment.start].y + yLength * r), 0);
-	/*console.debug("(" + p0.x + ", " + p0.y + ") and
-	(" + p1.x + ", " + p1.y + ") : " + "(" + point.x + ", " + point.y + ")");*/
+	console.debug("(" + p0.x + ", " + p0.y + ") and
+	(" + p1.x + ", " + p1.y + ") : " + "(" + point.x + ", " + point.y + ")");
 	return point;
 };
+*/
