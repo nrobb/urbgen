0f3f6929999e3796382a7c914d1be0aec1caf358
N Robb <shogastudio@gmail.com>
2014-07-12 17:24:20 +0100

    Add functions to divide quadrilaterals and to get length of an edge

--- js/UrbGen.js
+++ js/UrbGen.js
@@ -1,26 +1,31 @@
-// Defines a point, specified by a vec3
+// Defines a point, specified by x, y, and z coords
 var Point = function(x, y, z) {
   this.x = x;
   this.y = y;
   this.z = z;
 };
-// Defines an absolute edge, specified by a start and end vec3
+// Defines an absolute edge, specified by a start and end point
 var AbsEdge = function(start, end) {
   this.edges = [];
   this.start = start;
   this.end = end;
   this.getPoint = function(r) {
-    //return linearInterpolate(this, r);
-    return cosineInterpolate(this, r);
+    return linearInterpolate(this, r);
+    //return cosineInterpolate(this, r);
+  };
+  this.length = function() {
+    return Math.sqrt(Math.pow((this.end.x - this.start.x), 2) + Math.pow((this.end.y - this.start.y), 2));
   };
 };
 // Defines a relative edge, specified by a master edge, and start and end values
 var RelEdge = function(master, value0_1Start, value0_1End) {
+  this.startValue = value0_1Start;
+  this.endValue = value0_1End;
   this.start = master.getPoint(value0_1Start);
   this.end = master.getPoint(value0_1End);
   this.master = master;
   this.getPoint = function(r) {
-    return master.getPoint(this.start + r * (this.end - this.start));
+    return master.getPoint(this.startValue + r * (this.endValue - this.startValue));
   };
 };
 // Deines a composite edge, specified by a list of other edges
@@ -42,6 +47,58 @@ var CompEdge = function(edges) {
 // Defines a Quadrilateral, specified by four edges
 var Quad = function(edge1, edge2, edge3, edge4) {
   this.edges = [edge1, edge2, edge3, edge4];
+  this.quads = [];
+  this.area = function() { // THIS IS APPROX, DOES NOT RETURN ACTUAL AREA
+    return this.edges[0].length() * this.edges[1].length();
+  };
+};
+// Divides a Quadrilateral in two, adding the two new quads to the original
+var divideQuad = function(quad) {
+  if (Math.random() > 0.5) {
+    getNewVerticalQuads(quad);
+  } else {
+    getNewHorizontalQuads(quad);
+  }
+};
+// Create two new vertical quads
+var getNewVerticalQuads = function(quad) {
+  var startPoint = getLimitedRandom();
+  var endPoint = getLimitedRandom();
+  var start = quad.edges[0].getPoint(startPoint);
+  var end = quad.edges[3].getPoint(endPoint);
+  var newEdge = new AbsEdge(start, end);
+  var quad1 = new Quad(new RelEdge(quad.edges[0], 0, startPoint),
+                      newEdge,
+                      quad.edges[2],
+                      new RelEdge(quad.edges[3], 0, endPoint));
+  var quad2 = new Quad(new RelEdge(quad.edges[0], startPoint, 1),
+                      quad.edges[1],
+                      newEdge,
+                      new RelEdge(quad.edges[3], endPoint, 1));
+  quad.quads.push(quad1);
+  quad.quads.push(quad2);
+};
+// Create two new horizontal quads
+var getNewHorizontalQuads = function(quad) {
+  var startPoint = getLimitedRandom();
+  var endPoint = getLimitedRandom();
+  var start = quad.edges[2].getPoint(startPoint);
+  var end = quad.edges[1].getPoint(endPoint);
+  var newEdge = new AbsEdge(start, end);
+  var quad1 = new Quad(quad.edges[0],
+                      new RelEdge(quad.edges[1], 0, endPoint),
+                      new RelEdge(quad.edges[2], 0, startPoint),
+                      newEdge);
+  var quad2 = new Quad(newEdge,
+                      new RelEdge(quad.edges[1], endPoint, 1),
+                      new RelEdge(quad.edges[2], startPoint, 1),
+                      quad.edges[3]);
+  quad.quads.push(quad1);
+  quad.quads.push(quad2);
+};
+// Returns a random number in the range 0.3 - 0.7
+var getLimitedRandom = function() {
+  return (Math.random() * 2 + 4) / 10;
 };
 // Divides an edge into a composite edge, composed of n edges
 var makeCompositeEdge = function(edge, n) {
